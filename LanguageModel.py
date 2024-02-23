6eee3db363e461e796c4d38264f9407a65fac3ec
atornow <atornow22@gmail.com>
2024-02-23 14:59:40 -0800

    Rewrote entire forward backward pass as "mainPass" in ModelFunctions.py. Runs indefinitly through data, no epochs.

--- LanguageModel.py
+++ LanguageModel.py
@@ -1,77 +1,61 @@
 import numpy as np
 import ModelFunctions
 
-vocab = 75
+# Hyperparameters of model
 hiddenSize = 100
-file_path = 'SciFiData.txt'
-load = False
 learningRate = 0.1
-percentData = 0.01
-epochs = 100
-Inputs = ModelFunctions.inputLoader(file_path, load, percentData)
-dataSize = len(Inputs)
+batchSize = 25
 
+# Load data from txt and create dictionaries for one-hot encoding
+data = open('ScifiData.txt', 'r').read()
+chars = list(set(data))
+charToIndex = {ch: i for i, ch in enumerate(chars)}
+indexToChar = {i: ch for i, ch in enumerate(chars)}
+dataSize, vocab = len(data), len(chars)
+
+# Initialize weight matrices and bias vectors
 U = np.random.uniform(low=-0.1, high=0.1, size=(vocab, hiddenSize))
 V = np.random.uniform(low=-0.1, high=0.1, size=(hiddenSize, vocab))
 W = np.random.uniform(low=-0.1, high=0.1, size=(hiddenSize, hiddenSize))
-
 h = np.random.uniform(low=-0.3, high=0.3, size=(1, hiddenSize))
 b = np.zeros((1, hiddenSize))
 c = np.zeros((1, vocab))
-savedHidden = np.zeros((dataSize, hiddenSize))
-savedError = np.zeros(epochs)
-savedLogits = np.zeros((dataSize, vocab))
 
-for epoch in range(0, epochs):
+# Initialize weight and vector derivative memories for adagrad
+memdOfB = np.zeros_like(b)
+memdOfC = np.zeros_like(c)
+memdOfU = np.zeros_like(U)
+memdOfW = np.zeros_like(W)
+memdOfV = np.zeros_like(V)
 
-    # Forward pass
-    extraError = np.zeros(dataSize)
-    for char in range(0, dataSize - 1):
-        x = np.dot(Inputs[char], U)
-        a = b + np.dot(h, W) + x
-        h = ModelFunctions.hiddenFunction(a)
+# Get starting loss point
+average_loss = -np.log(1.0 / vocab) * batchSize  # Initial loss at iteration 0
+iteration_counter, data_pointer = 0, 0
+while True:
+    # Keep track of current iteration and place in batch
 
-        savedHidden[char] = h
-        output = c + np.dot(h, V)
-        savedLogits[char] = output
+    if data_pointer + batchSize + 1 >= len(data) or iteration_counter == 0:
+        prevHidden = np.zeros((1, hiddenSize))
+        data_pointer = 0
 
-        loss = ModelFunctions.crossEntropy(ModelFunctions.softMax(output), Inputs[char + 1])
-        savedError[epoch] += loss
-        extraError[char] = loss
+    # Create a batch of batchSize and equal sized target array to compare
+    Batch = [charToIndex[char] for char in data[data_pointer:data_pointer + batchSize]]
+    Targets = [charToIndex[char] for char in data[data_pointer + 1:data_pointer + batchSize + 1]]
 
-    lossOfC = np.zeros((1, vocab))
-    lossOfB = np.zeros((1, hiddenSize))
-    lossOfU = np.random.uniform(low=-0.3, high=0.3, size=(vocab, hiddenSize))
-    lossOfV = np.random.uniform(low=-0.3, high=0.3, size=(hiddenSize, vocab))
-    lossOfW = np.random.uniform(low=-0.3, high=0.3, size=(hiddenSize, hiddenSize))
+    # Run batch through full forward/backward pass to get derivatives
+    loss, dOfU, dOfW, dOfV, dOfB, dOfC, prevHidden = ModelFunctions.mainPass(Batch, Targets, prevHidden, U, W, V, b, c)
 
-    nextLOfH = np.zeros((1, hiddenSize))
-    nextH = np.zeros((1, hiddenSize))
+    # Average loss over batches and print it every 100 iterations
+    average_loss = average_loss * 0.999 + loss * 0.001
+    if iteration_counter % 100 == 0:
+        print('Iteration ' + str(iteration_counter) + ' loss: ' + str(average_loss))
 
-    # Backwards pass through sequence
-    for char in range(dataSize, 0):
-        hOfH = np.dot(np.transpose(W), np.diag(1 - np.square(nextH)))
-        lossOfO = ModelFunctions.derivativeLofO(savedLogits[char], Inputs[char])
+    # Update the weights and bias with adagrad method
+    for parameter, gradient, memory in zip([U, W, V, b, c], [dOfU, dOfW, dOfV, dOfB, dOfC],
+                                           [memdOfU, memdOfW, memdOfV, memdOfB, memdOfC]):
+        memory += gradient ** 2
+        parameter += -learningRate * gradient / np.sqrt(memory + 1e-8)
 
-        lossOfH = np.dot(nextLOfH, hOfH) + np.dot(lossOfO, np.transpose(V))
-        nextLOfH = lossOfH
-        nextH = savedHidden[char]
 
-        lossOfC += lossOfO
-        lossOfB += np.dot(lossOfH, np.diag(1 - (np.square(savedHidden[char]))))
-        lossOfV += np.dot(np.transpose(savedHidden[char]), lossOfO)
-        lossOfW += np.dot(np.transpose(savedHidden[char - 1]), np.dot(lossOfH, np.diag(1 - np.square(savedHidden[char]))))
-        lossOfU += np.dot(np.transpose(Inputs[char]), np.dot(lossOfH, np.diag(1 - np.square(savedHidden[char]))))
-
-    c = c - learningRate * (lossOfC / dataSize)
-    b = b - learningRate * (lossOfB / dataSize)
-    V = V - learningRate * (lossOfV /dataSize)
-    W = W - learningRate * (lossOfW / dataSize)
-    U = U - learningRate * (lossOfU / dataSize)
-
-savedError = savedError / dataSize
-
-print(dataSize)
-
-ModelFunctions.plot_error_set(savedError)
-ModelFunctions.plot_error_set(extraError)
+    data_pointer += batchSize
+    iteration_counter += 1
--- ModelFunctions.py
+++ ModelFunctions.py
@@ -29,16 +29,64 @@ def crossEntropy(Actual, Desired):
     loss = -np.sum(Desired * np.log(Actual))
     return loss
 
-def inputLoader(file_path, load, percentData):
-    if load:
-        Inputs = TrainingDataLoader.file_to_binary_matrix(file_path)
-        np.save('SavedInputs.npy', Inputs)
-        return Inputs
 
-    else:
-        Inputs = np.load('SavedInputs.npy')
-        Inputs = Inputs[:int(len(Inputs) * percentData)]
-        return Inputs
+def mainPass(Batch, Targets, prevHidden, U, W, V, b, c):
+
+    oneHot = {}
+    savedHiddens = {}
+    savedLogits = {}
+    savedProbs = {}
+    savedHiddens[-1] = np.copy(prevHidden)
+    loss = 0
+
+    # Forward pass through Batch
+    for char in range(len(Batch)):
+
+        oneHot[char] = np.zeros_like(c)  # encode in 1-of-vocab representation
+        oneHot[char][0, Batch[char]] = 1
+
+        savedHiddens[char] = hiddenFunction(b + np.dot(savedHiddens[char - 1], W) + np.dot(oneHot[char], U))
+        savedLogits[char] = c + np.dot(savedHiddens[char], V)
+        savedProbs[char] = softMax(savedLogits[char])
+
+        prob_array_for_char = savedProbs[char]
+        target_prob = prob_array_for_char[0][Targets[char]]
+        loss += -np.log(target_prob)
+
+    dOfB = np.zeros_like(b)
+    dOfC = np.zeros_like(c)
+    dOfU = np.zeros_like(U)
+    dOfW = np.zeros_like(W)
+    dOfV = np.zeros_like(V)
+
+    dNextHidden = np.zeros_like(savedHiddens[0])
+
+    # Backwards pass through sequence
+    for char in reversed(range(len(Batch))):
+
+        # Helper variables for getting derivatives over batch
+        dLogit = np.copy(savedProbs[char])
+        dLogit[0][Targets[char]] -= 1
+        dOfH = dNextHidden + np.dot(dLogit, np.transpose(V))
+        dOfHRaw = np.dot(dOfH, np.diag((1 - (np.square(savedHiddens[char]))).flatten()))
+
+        # Calculate and sum derivatives over batch
+        dOfB += dOfHRaw
+        dOfC += dLogit
+        dOfU += np.dot(np.transpose(oneHot[char]), dOfHRaw)
+        dOfW += np.dot(np.transpose(savedHiddens[char - 1]), dOfHRaw)
+        dOfV += np.dot(np.transpose(savedHiddens[char]), dLogit)
+
+        # Update change in hidden with respect to previous hidden
+        dNextHidden = np.dot(dOfHRaw, W.T)
+
+    # Clip values in derivatives to prevent explosions in gradient
+    for dParam in [dOfU, dOfW, dOfV, dOfB, dOfC]:
+        np.clip(dParam, -5, 5, out=dParam)
+
+    # Return summed loss and derivatives aswell as the last state vector for next pass
+    return loss, dOfU, dOfW, dOfV, dOfB, dOfC, savedHiddens[len(Batch) - 1]
+
 
 def plot_error_set(ErrorSet):
     # Determine the size of ErrorSet
